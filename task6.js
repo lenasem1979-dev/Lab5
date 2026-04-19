ef34b9301c592882dcb9fff47710ae57c7fe7789
LAPTOP-95RBQN49\User <Lena.Sem1979@gmail.com>
2026-04-19 12:09:38 +0300

    Лабораторна робота 5 завдання 6

---
+++ task6.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelectorAll(".card").forEach(card => {
+
+        card.addEventListener("click", () => {
+            card.classList.toggle("flipped");
+        });
+
+    });
+
+});
